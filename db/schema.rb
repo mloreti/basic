0a9bb31986225cfbfa4ceaa98566063df5bd789c
Mickey Loreti <mloreti10@gmail.com>
2018-03-09 00:50:28 -0800

    Add first and last name columns

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 20180309075230) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
