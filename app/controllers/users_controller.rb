f08819834a887333aef5c0f73206d2bacee9e426
Mickey Loreti <mloreti10@gmail.com>
2018-03-09 00:22:29 -0800

    add methods for user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,38 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :destroy]
+
   def show
+    if @user
+      render :show
+    else
+       render json: { "error":"User not found" }, status: 422
+    end
+  end
+ 
+   def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show
+    else
+      render :errors, status: 422
+    end
+  end
+ 
+  def destory
+    if @user && @user.destroy
+      render status: 200
+    else
+      render :errors, status: 422
+    end
   end
 
-  def create
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 
-  def destroy
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
